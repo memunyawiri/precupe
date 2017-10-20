9ac65ab68e4fb60f3665bfb097e9e7b1d3744009
Mutsa <mutsa.evelyn@hotmail.com>
2017-10-20 16:21:46 +0100

    Mores styling added + recipe buttons

--- src/App.js
+++ src/App.js
@@ -9,20 +9,25 @@ class App extends Component {
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Precüpe</h1>
+          <h1 className="App-title">precüpe</h1>
         </header>
 
         <p className="App-intro">
           Add your own recipes, new & old or load the url of a recipe from your favourite foodie blogs and sites.
         </p>
 
-        <div className="Recipes">
-        // Interpolate recipes here
+        <div className= "Recipe-Buttons">
+        <input class='buttons' type='submit' value='Add New Recipe'/>
+        <input class='buttons' type='submit' value='Add Existing Recipe'/>
         </div>
 
-        <div className="Footer">
-        // Footer to go here
-        </div>
+        <body className="Recipes">
+        // Interpolate recipes here
+        </body>
+
+        <footer className="App-footer">
+        Copyright &copy; 2017 &middot; Created by Mutsa Munyawiri
+        </footer>
       </div>
     );
   }
